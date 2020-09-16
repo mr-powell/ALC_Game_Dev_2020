7e14caa2f51544a0e430cc294a759a5ec1f1c0a3
Powell <preston.powell@nebo.edu>
2020-09-16 09:09:14 -0600

    Worked on full drive mech

--- Prototype 1 - Driving Simulator/Assets/Scripts/PlayerController.cs
+++ Prototype 1 - Driving Simulator/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 20;
+    public float turnSpeed = 50;
+
+    public float horizontalInput;
+    public float forwardInput; 
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +19,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
         // Moves the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);    
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
