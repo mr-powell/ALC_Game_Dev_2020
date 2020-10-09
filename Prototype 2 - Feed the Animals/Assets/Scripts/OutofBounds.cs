8892e3e077138beae4bcdafdc7a8e592aae0fdc5
Powell <preston.powell@nebo.edu>
2020-10-09 15:15:05 -0600

    Added boundries and shoot mechanic

---
+++ Prototype 2 - Feed the Animals/Assets/Scripts/OutofBounds.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutofBounds : MonoBehaviour
+{
+    public float topBounds = 35.0f;
+    public float lowerBounds = -15.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z > topBounds)
+        {
+            Destroy(gameObject);
+        }
+
+        if(transform.position.z < lowerBounds)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
