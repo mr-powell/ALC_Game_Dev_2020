ebb3dcacc06c7a7da26d7a42199e61e6c27a658f
Powell <preston.powell@nebo.edu>
2020-10-12 09:09:34 -0600

    Prototype 2 Update

--- Prototype 2 - Feed the Animals/Assets/Scripts/PlayerController.cs
+++ Prototype 2 - Feed the Animals/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 15.0f;
+    public GameObject projectilePrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -19,17 +20,26 @@ public class PlayerController : MonoBehaviour
     {
         //Collect Input Data on Horizontal controls
         horizontalInput = Input.GetAxis("Horizontal");
+
         //Moves the player left and right
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
         //Places a boundry to the left 
         if(transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
+
         //Places a boundry to the right
         if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab,transform.position,projectilePrefab.transform.rotation);
         }
         
     }
